86e08e12871a493d0e580f7841f3b756c6c81939
Marek Rydlewski <marek.rydlewski@hotmail.com>
2015-11-12 13:06:39 +0100

    Refactor part 1

--- scan.py
+++ scan.py
@@ -9,51 +9,66 @@ import numpy as np
 import argparse
 import cv2
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required = True,
-	help = "Path to the image to be scanned")
-args = vars(ap.parse_args())
 
-# load the image and compute the ratio of the old height
-# to the new height, clone it, and resize it
-image = cv2.imread(args["image"])
-ratio = image.shape[0] / 500.0
-orig = image.copy()
-image = imutils.resize(image, height = 500)
+def load_image_from_args():
+    # construct the argument parser and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required=True, help="Path to the image to be scanned")
+    args = vars(ap.parse_args())
+    image = cv2.imread(args["image"])
+    return image
 
-# convert the image to grayscale, blur it, and find edges
-# in the image
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(gray, (5, 5), 0)
-edged = cv2.Canny(gray, 75, 200)
 
+def resize_image(image, desired_height):
+    # load the image and compute the ratio of the old height
+    # to the new height, clone it, and resize it
+    ratio = image.shape[0] / desired_height
+    orig = image.copy()
+    image = imutils.resize(image, height = desired_height)
+    return image, orig, ratio
+
+def image_to_grey_blur_canny_edges(image):
+    # convert the image to grayscale, blur it, and find edges
+    # in the image
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    edged = cv2.Canny(gray, 75, 200)
+    return edged
+
+def edged_image_to_contours(edged):
+    # find the contours in the edged image, keeping only the
+    # largest ones, and initialize the screen contour
+    _, cnts, _ = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
+    # loop over the contours
+    for c in cnts:
+        # approximate the contour
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+        # if our approximated contour has four points, then we
+        # can assume that we have found our screen
+        if len(approx) == 4:
+            return approx
+            # screenCnt = approx
+            # break
+    return -1
+
+image = load_image_from_args()
+image, orig, ratio = resize_image(image, 500)
+edged = image_to_grey_blur_canny_edges(image)
 # show the original image and the edge detected image
-print ("STEP 1: Edge Detection")
-cv2.imshow("Image", image)
+print("STEP 1: Edge Detection")
+# cv2.imshow("Image", image)
 cv2.imshow("Edged", edged)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-# find the contours in the edged image, keeping only the
-# largest ones, and initialize the screen contour
-_, cnts, _ = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+screenCnt = edged_image_to_contours(edged)
 
-# loop over the contours
-for c in cnts:
-	# approximate the contour
-	peri = cv2.arcLength(c, True)
-	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-
-	# if our approximated contour has four points, then we
-	# can assume that we have found our screen
-	if len(approx) == 4:
-		screenCnt = approx
-		break
 
 # show the contour (outline) of the piece of paper
-print ("STEP 2: Find contours of paper")
+print("STEP 2: Find contours of paper")
 cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
 cv2.imshow("Outline", image)
 cv2.waitKey(0)
@@ -66,11 +81,11 @@ warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
 # convert the warped image to grayscale, then threshold it
 # to give it that 'black and white' paper effect
 warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
-warped = threshold_adaptive(warped, 250, offset = 10)
+warped = threshold_adaptive(warped, 250, offset=10)
 warped = warped.astype("uint8") * 255
 
 # show the original and scanned images
-print ("STEP 3: Apply perspective transform")
-cv2.imshow("Original", imutils.resize(orig, height = 650))
-cv2.imshow("Scanned", imutils.resize(warped, height = 650))
+print("STEP 3: Apply perspective transform")
+cv2.imshow("Original", imutils.resize(orig, height=650))
+cv2.imshow("Scanned", imutils.resize(warped, height=650))
 cv2.waitKey(0)
